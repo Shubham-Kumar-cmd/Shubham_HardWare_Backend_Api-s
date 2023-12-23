01bde2262540a2c8d0cf0e5fcd6d9cca727d926f
Shubham-Kumar-cmd <kumar53shubham@gmail.com>
2023-12-23 16:16:59 +0530

    added security on swagger using springdoc migration

--- src/main/java/com/shubham/hardware/config/SwaggerConfig.java
+++ src/main/java/com/shubham/hardware/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.shubham.hardware.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -84,7 +87,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI(){
+
+        String schemeName="bearerScheme";
+
         OpenAPI openAPI = new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(schemeName)
+                )
+                .components(new Components()
+                        .addSecuritySchemes(schemeName,new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .bearerFormat("JWT")
+                                .scheme("bearer")
+                        )
+                )
                 .info(new Info().title("Shubham Hardware Backend : APIS")
                         .description("This is a backend api's for creating a online shop for selling products!!")
                         .version("1.0v")
